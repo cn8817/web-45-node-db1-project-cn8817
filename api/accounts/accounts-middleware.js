4287da7ac889659c42d12a74eac81d74a24cab8c
cn8817 <christinenguyen1433@gmail.com>
2021-09-07 12:00:08 -0700

    bugs

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -6,15 +6,15 @@ exports.checkAccountPayload = (req, res, next) => {
   const { name, budget } = req.body
   if(name === undefined || budget === undefined ) {
     error.message = "name and budget are required"
-  } else if (name.trim().length < 3 || name.trim().length > 100) {
-    error.message = "name of account must be between 3 and 100"
   } else if (typeof name !== 'string') {
     error.message = "name of account must be a string"
-} else if (typeof budget !== 'number' || isNaN(budget)){
-  error.message = "budget of account must be a number"
-} else if (budget < 0 || budget > 1000000 ) {
-  error.message = "budget of account is too large or too small"
-}
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = "name of account must be between 3 and 100"
+  } else if (typeof budget !== 'number' || isNaN(budget)){
+    error.message = "budget of account must be a number"
+  } else if (budget < 0 || budget > 1000000 ) {
+    error.message = "budget of account is too large or too small"
+  }
   if (error.message) {
     next(error)
   } else {
