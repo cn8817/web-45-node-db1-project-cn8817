eb9da64d18ad2ff74880fe412deecdaa8c345890
cn8817 <christinenguyen1433@gmail.com>
2021-09-07 10:39:20 -0700

    made models and some middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,29 @@
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { name, budget } = req.body
+  if(!name || !name.trim() || !budget ) {
+    res.status(400).json({
+      message: "name and budget are required"
+    })
+  } else if (name.trim() < 3 || name.trim() > 100) {
+    res.status(400).json({
+      message: "name of account must be between 3 and 100"
+    })
+  } else if (typeof name !== 'string') {
+    res.status(400).json({
+      message: "name of account must be a string"
+  })
+} else if (typeof budget !== 'number'){
+  res.status(400).json({
+    message: "budget of account must be a number"
+})
+} else if (budget < 0 || budget > 1000000 ) {
+  res.status(400).json({
+    message: "budget of account is too large or too small"
+})
+} else {
+  req.name = name.trim()
+  req.budget = budget
+  next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
